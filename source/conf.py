0adbcf1e1a3ee0ed9c8495dfda2eaf82930e6f4a
Dmitry Kozlov <jusper@yandex.ru>
2021-02-16 18:14:47 +0300

    new theme

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ import os
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.4.1'
 
 # Options for extensions.
 disqus_shortname = 'plumsail'  # Add this line to conf.py.
@@ -32,7 +32,8 @@ disqus_shortname = 'plumsail'  # Add this line to conf.py.
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []  # 'sphinxcontrib.disqus', 'rst2pdf.pdfbuilder']  # Add to this list.
+extensions = ['sphinx_sitemap']  # 'sphinxcontrib.disqus', 'rst2pdf.pdfbuilder']  # Add to this list.
+html_baseurl = 'https://plumsail.com/docs/documents/v1.x/' #for sitemap
 
 #pdf_documents = [('index', u'rst2pdf', u'Sample rst2pdf doc', u'Your Name'),]
 
@@ -51,7 +52,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Plumsail Org Chart'
+project = 'Plumsail Documents'
 copyright = '2020, Plumsail'
 author = 'Plumsail'
 
@@ -113,49 +114,49 @@ todo_include_todos = False
 # -- Options for HTML output ----------------------------------------------
 #import sphinx_rtd_theme
 
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "basic"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = { 
-#	'display_version': False,
-#    'sticky_navigation': True,
-#	'analytics_id': 'UA-36215023-2',
-#}
+# html_theme_options = { 
+# 	'display_version': False,
+#     'sticky_navigation': True,
+# 	'analytics_id': 'UA-36215023-2',
+# }
 
 #html_theme = "sphinx_rtd_theme"
 #html_theme_path = ["_themes\sphinx_librato_theme", ]
 
 #---Other theme
-import guzzle_sphinx_theme
+# import guzzle_sphinx_theme
 
 # Adds an HTML table visitor to apply Bootstrap table classes
-html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+# html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
+# html_theme_path = guzzle_sphinx_theme.html_theme_path()
+# html_theme = 'guzzle_sphinx_theme'
 
 # Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
+# extensions.append("guzzle_sphinx_theme")
 
 # Guzzle theme options (see theme.conf for more information)
-html_theme_options = {
-    # Set the name of the project to appear in the sidebar
-    "project_nav_name": "Plumsail Org Chart",
-     # Set your Disqus short name to enable comments
-    "disqus_comments_shortname": "plumsail",
-
-    # Set you GA account ID to enable tracking
-    "google_analytics_account": "UA-36215023-2",
-    "base_url": "https://plumsail.com/docs/orgchart/v1.x/" #for sitemap
-}
+# html_theme_options = {
+#     # Set the name of the project to appear in the sidebar
+#     "project_nav_name": "Plumsail Documents",
+#      # Set your Disqus short name to enable comments
+#     "disqus_comments_shortname": "plumsail",
+# 
+#     # Set you GA account ID to enable tracking
+#     "google_analytics_account": "UA-36215023-2",
+#     "base_url": "https://plumsail.com/docs/documents/v1.x/" #for sitemap
+# }
 
 html_sidebars = {'**': ['searchbox.html','globaltoc.html']}
 #-----
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster' #by default
+html_theme = 'basic'
 
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -163,7 +164,7 @@ html_sidebars = {'**': ['searchbox.html','globaltoc.html']}
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-#html_title = 'Plumsail Org Chart'
+#html_title = 'Plumsail Documents'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -180,13 +181,14 @@ html_sidebars = {'**': ['searchbox.html','globaltoc.html']}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+# 
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides_guzzle.css',  # overrides for wide tables in RTD theme
-        ],
-    }
+# html_context = {
+#     'css_files': [
+#         '_static/theme_overrides_guzzle.css',  # overrides for wide tables in RTD theme
+#         ],
+#     }
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -251,7 +253,7 @@ html_show_sphinx = False
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PlumsailOrgChart'
+htmlhelp_basename = 'PlumsailDocuments'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -273,7 +275,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PlumsailOrgChart.tex', 'Plumsail Org Chart Documentation',
+    (master_doc, 'PlumsailDocuments.tex', 'Plumsail Documents Documentation',
      'Plumsail', 'manual'),
 ]
 
@@ -303,7 +305,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sharepoint-orgchart', 'Plumsail Org Chart Documentation',
+    (master_doc, 'documents', 'Plumsail Documents Documentation',
      [author], 1)
 ]
 
@@ -317,8 +319,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sharepoint-orgchart', 'Plumsail Org Chart Documentation',
-     author, 'sharepoint-orgchart', 'Oneline description of project.',
+    (master_doc, 'documents', 'Plumsail Documents Documentation',
+     author, 'documents', 'Oneline description of project.',
      'Miscellaneous'),
 ]
 
